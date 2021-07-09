285ca47b4b4cfe24c857d320712f3cc2952b9977
kosweb <koleitdev@gmail.com>
2021-07-09 19:35:10 +0300

    mobile index style alm done

--- source/js/slider-toggles.js
+++ source/js/slider-toggles.js
@@ -1,11 +1,32 @@
 const advantagesItems = document.querySelectorAll('.advantages__item');
 const advantagesToggles = document.querySelectorAll('.advantages__toggles .slider__toggle');
+const reviewsItems = document.querySelectorAll('.reviews__item');
+const reviewsToggles = document.querySelectorAll('.reviews__toggles .slider__toggle');
 
 advantagesToggles.forEach(function(el, i) {
 	el.onclick = function() {
+		for (let advantagesToggle of advantagesToggles) {
+			advantagesToggle.classList.remove('slider__toggle--active')
+		};
+		el.classList.add('slider__toggle--active');
+
 		for (let advantagesItem of advantagesItems) {
 			advantagesItem.classList.remove('slider__item--show')
 		};
 		advantagesItems[i].classList.add('slider__item--show');
+	};
+});
+
+reviewsToggles.forEach(function(el, i) {
+	el.onclick = function() {
+		for (let reviewsToggle of reviewsToggles) {
+			reviewsToggle.classList.remove('slider__toggle--active')
+		};
+		el.classList.add('slider__toggle--active');
+
+		for (let reviewsItem of reviewsItems) {
+			reviewsItem.classList.remove('slider__item--show')
+		};
+		reviewsItems[i].classList.add('slider__item--show');
 	};
 });
